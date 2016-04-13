9bf0d5a9ef300c488148c1a2cdb413442a61087f
AdrianTao <Adrian@taojundeMacBook-Air.local>
2016-04-13 15:26:24 +0800

    add food
    添加方法，生成随机位置的食物

--- snake/snake.js
+++ snake/snake.js
@@ -1,4 +1,6 @@
-function Snake(){
+function Snake(width,height){
+	this.width = width;
+	this.height = height;
 	this.battle = null;
 	this.body = [];
 }
@@ -6,9 +8,9 @@ Snake.prototype = {
 	init:function(id){
 		this.box = document.getElementById(id);
 		var ul = "";
-		for(var i=0 ; i<10 ; i++){
+		for(var i=0 ; i<this.height ; i++){
 			var row = '';
-			for(var j=0 ; j<10 ; j++){
+			for(var j=0 ; j<this.width ; j++){
 				if(j==0){
 					row += "<ul><li id='" + "li_"+(i*10+j+1) + "'></li>";
 				}else{
@@ -21,9 +23,25 @@ Snake.prototype = {
 			ul += row;
 		}
 		this.box.innerHTML = ul;
+	},
+	random:function(){
+		for( ; ; ){
+			var num = Math.floor(Math.random() * 100 + 1);
+			var flag = true;
+			for(var i=0 ; i<this.body.length ; i++){
+				var sbody = this.body[i].className.split("_")[1];
+				if(num == sbody){
+					flag = false;
+				}
+			}
+			if(flag){
+				document.getElementById("li_" + num).className = "on";
+				break;
+			}
+		}
 	}
 }
 window.onload = function(){
-	var snake = new Snake();
+	var snake = new Snake(10,10);
 	snake.init("battlefield");
 }
