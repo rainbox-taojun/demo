ff535ce1dbb20e85b7bbe21cabbce8b1e54be890
AdrianTao <Adrian@taojundeMacBook-Air.local>
2016-04-13 15:03:21 +0800

    add snake
    贪吃蛇小游戏，添加棋盘

--- snake/snake.js
+++ snake/snake.js
@@ -5,9 +5,25 @@ function Snake(){
 Snake.prototype = {
 	init:function(id){
 		this.box = document.getElementById(id);
-		var ul = document.createElement("ul");
+		var ul = "";
 		for(var i=0 ; i<10 ; i++){
 			var row = '';
+			for(var j=0 ; j<10 ; j++){
+				if(j==0){
+					row += "<ul><li id='" + "li_"+(i*10+j+1) + "'></li>";
+				}else{
+					row += "<li id='" + "li_" + (i*10 +j+1) + "'></li>";
+				}
+				if(j == 9){
+					row += "</ul>";
+				}
+			}
+			ul += row;
 		}
+		this.box.innerHTML = ul;
 	}
+}
+window.onload = function(){
+	var snake = new Snake();
+	snake.init("battlefield");
 }
