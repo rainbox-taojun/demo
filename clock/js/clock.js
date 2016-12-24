99a53533b2543ebb9c4e71a73dd12bc351881a18
Adrian tao <taojun1994@gmail.com>
2016-12-24 20:10:48 +0800

    Initially completed

--- clock/js/clock.js
+++ clock/js/clock.js
@@ -5,27 +5,45 @@ var clock = function(boxName){
 		var myDate,
 			hour,
 			minute,
-			second;
+			second,
+			angle;
 		setInterval(function(){
 			myDate = new Date();
 			hour = myDate.getHours();
 			minute = myDate.getMinutes();
 			second = myDate.getSeconds();
+			angle = second*6;
 
-			$(boxName+" .hour").html(hour<10?("0"+hour):hour);
-			$(boxName+" .minute").html(minute<10?("0"+minute):minute);
-			$(boxName+" .second").html(second<10?("0"+second):second);
+			$("span").html((hour<10?"0"+hour:hour)+":"+(minute<10?"0"+minute:minute));
 
-			that.setLongshadow();
+			that.mySetLongshadow({
+				box : ".clock",
+				colorShadow : "#333",
+				sizeShadow : 1000,
+				angleShadow : angle
+			});
 		},500);
+		
+		console.log()
 	}
 
-	that.setLongshadow = function(){
-		$(".clock li").longShadow({
-			colorShadow: '#333',
-			sizeShadow: 1000,
-			directionShadow: 'bottom'
-		});
+	that.mySetLongshadow = function(obj){
+		var parent = $(obj.box),
+			colorShadow = obj.colorShadow || "#333",
+			sizeShadow = obj.sizeShadow || 100,
+			angleShadow  = obj.angleShadow,
+			textShadow = "rgb(51, 51, 51) 0px 0px 0px",
+			x,
+			y;
+
+		parent.css("text-shadow","rgb(51, 51, 51) 0px 0px 0px");
+
+		for(var i=1;i<sizeShadow;i++){
+			x = i * Math.cos((angleShadow-90) * Math.PI / 180) + "px";
+			y = i * Math.sin((angleShadow-90) * Math.PI / 180) + "px";
+			textShadow += "," + colorShadow + " " + x + " " + y + " 0px";
+		}
+		parent.css("text-shadow",textShadow);
 	}
 
 
